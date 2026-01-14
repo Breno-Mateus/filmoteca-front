47e0182e81f0f1484fb508c446d04b204f029575
Breno-Mateus <brenomateus36@gmail.com>
2026-01-14 18:57:20 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <header className="bg-gray-800">
+        <h1 className="text-white text-3xl font-bold p-4">&#127916; Filmoteca</h1>
+      </header>
+    </>
+  )
+}
+
+export default App;
